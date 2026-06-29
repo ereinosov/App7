280efb3927643de51571b7f0993dfacc15891d0e
Eduardo Reinoso <ereinosov@uteq.edu.ec>
2026-06-29 16:52:04 -0500

    Fin

--- app/src/main/java/com/example/app7/MainActivity.java
+++ app/src/main/java/com/example/app7/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.app7;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -11,7 +12,10 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;import com.google.android.gms.maps.OnMapReadyCallback;import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 GoogleMap mapa;
@@ -39,4 +43,41 @@ GoogleMap mapa;
             CameraUpdateFactory
                     .newLatLngZoom(new LatLng(-1.0125, -79.4693), 17);
          mapa.moveCamera(camUpd1);
-    }}
+
+//    LatLng metlife = new LatLng(40.8122, -74.0764);
+
+//    CameraPosition camPos = new CameraPosition.Builder()
+//            .target(metlife)
+//            .zoom(18)      // Zoom adecuado para ver el estadio completo
+//            .bearing(0)    // 0 es Norte (visto desde abajo hacia arriba)
+//            .tilt(65)      // Inclinación alta para ver perspectiva
+//            .build();
+//    CameraUpdate camUpd3 =
+//            CameraUpdateFactory.newCameraPosition(camPos);
+//    mapa.animateCamera(camUpd3);
+
+    PolylineOptions lineas = new
+            PolylineOptions()
+
+            .add(new LatLng(-1.0123, -79.4672))
+
+            .add(new LatLng(-1.0119, -79.4717))
+
+            .add(new LatLng(-1.0130, -79.4716))
+
+            .add(new LatLng(-1.0132, -79.4674))
+
+            .add(new LatLng(-1.0123, -79.4672));
+
+    lineas.width(8);
+
+    lineas.color(Color.RED);
+
+    mapa.addPolyline(lineas);
+
+    LatLng punto = new LatLng(-1.0125,
+            -79.4693);
+    mapa.addMarker(new
+            MarkerOptions().position(punto)
+            .title("UTEQ"));
+}}
